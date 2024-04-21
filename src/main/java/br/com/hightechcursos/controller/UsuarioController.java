eb610f9ae657578294f956b3a42ce4d20a2da51d
celsobritodev <celsinhobrito@gmail.com>
2024-04-21 10:01:59 -0300

    atualizacao

---
+++ src/main/java/br/com/hightechcursos/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package br.com.hightechcursos.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class UsuarioController
+ */
+ @WebServlet("/usucontroller.do")
+public class UsuarioController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UsuarioController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Chamando o Metodo GET");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Chamando o metodo POST");
+		doGet(request, response);
+	}
+
+}
